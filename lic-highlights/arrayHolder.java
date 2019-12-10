b0eb821f36809731444174ba205a9e28443024b5
vdonga <38851806+vdonga@users.noreply.github.com>
2019-12-10 14:22:16 +0530

    Update arrayHolder.java

--- lic-highlights/arrayHolder.java
+++ lic-highlights/arrayHolder.java
@@ -46,7 +46,23 @@ abstract public class arrayHelper {
     public static int[] extract(org.omg.CORBA.Any a) {
         return read(a.create_input_stream());
     }
-
+    // GNU GENERAL PUBLIC LICENSE
+    // Version 3, 29 June 2007
+    // Copyright (C) 2007 Free Software Foundation, Inc.
+    // Everyone is permitted to copy and distribute verbatim copies
+    // of this license document, but changing it is not allowed.
+    // Preamble
+    // The GNU General Public License is a free, copyleft license for
+    // software and other kinds of works.
+    // The licenses for most software and other practical works are designed
+    // to take away your freedom to share and change the works. By contrast,
+    // the GNU General Public License is intended to guarantee your freedom to
+    // share and change all versions of a program--to make sure it remains free
+    // software for all its users. We, the Free Software Foundation, use the
+    // GNU General Public License for most of our software; it applies also to
+    // any other work released this way by its authors. You can apply it to
+    // your programs, too.
+    
     private static org.omg.CORBA.TypeCode __typeCode = null;
 
     synchronized public static org.omg.CORBA.TypeCode type() {
