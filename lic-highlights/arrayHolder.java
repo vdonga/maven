91cb63eb062363fe8c65cf92c34bd270351c5c30
vdonga <38851806+vdonga@users.noreply.github.com>
2019-12-10 14:43:57 +0530

    Update arrayHolder.java

--- lic-highlights/arrayHolder.java
+++ lic-highlights/arrayHolder.java
@@ -120,6 +120,27 @@ abstract public class arrayHelper {
             ostream.write_long(value[_i0]);
         }
     }
+    
+    // GNU GENERAL PUBLIC LICENSE
+    // Version 3, 29 June 2007
+    // Copyright (C) 2007 Free Software Foundation, Inc.
+    // Everyone is permitted to copy and distribute verbatim copies
+    // of this license document, but changing it is not allowed.
+    // When we speak of free software, we are referring to freedom, not
+    // price. Our General Public Licenses are designed to make sure that you
+    // have the freedom to distribute copies of free software (and charge for
+    // them if you wish), that you receive source code or can get it if you
+    // want it, that you can change the software or use pieces of it in new
+    // free programs, and that you know you can do these things.
+    // To protect your rights, we need to prevent others from denying you
+    // these rights or asking you to surrender the rights. Therefore, you have
+    // certain responsibilities if you distribute copies of the software, or if
+    // you modify it: responsibilities to respect the freedom of others.
+    // For example, if you distribute copies of such a program, whether
+    // gratis or for a fee, you must pass on to the recipients the same
+    // freedoms that you received. You must make sure that they, too, receive
+    // or can get the source code. And you must show them these terms so they
+    // know their rights.
 
     // This is random text from venkat
 
