06e202137bededa9fca597803f0dbf72a150fe1b
vdonga <38851806+vdonga@users.noreply.github.com>
2019-12-12 11:16:42 +0530

    Update arrayHolder.java

--- lic-highlights/arrayHolder.java
+++ lic-highlights/arrayHolder.java
@@ -155,4 +155,17 @@ Copyright (C) 2007 Free Software Foundation, Inc.
 Everyone is permitted to copy and distribute verbatim copies
 of this license document, but changing it is not allowed.
 
+        // Standard License Header
+    // Copyright (C) 2019 Venkat Ram Donga
+    // This program is free software: you can redistribute it and/or modify it under
+    // the terms of the GNU General Public License as published by the Free Software
+    // Foundation, version 3.
+
+    // This program is distributed in the hope that it will be useful, but WITHOUT
+    // ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    // FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+    // details.
+
+    // You should have received a copy of the GNU General Public License along with
+    // this program. If not, see <https://www.gnu.org/licenses/>.
 }
